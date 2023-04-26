9c8f59b04e1a4c3535a782fcc27d00f14ce633fd
RedsTom <tom.frefre.13@gmail.com>
2023-04-26 16:18:49 +0200

    Added nvim installation

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+# Copy configurations
+cp -r ./ ~/.config/
+
 # Install all the basic apps
 apt install -y policykit-1-gnome i3-wm flameshot rofi polybar nitrogen
 
@@ -5,5 +8,8 @@ apt install -y policykit-1-gnome i3-wm flameshot rofi polybar nitrogen
 apt install cargo -y
 cargo install alacritty 
 
-# Copy configurations
-cp -r ./ ~/.config/
+# Install neovim
+apt install neovim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+nvim -c 'PlugInstall'
