dcc8a7266eb31fab848b7214bc9633bc3cd0d129
RedsTom <tom.frefre.13@gmail.com>
2023-04-26 16:20:16 +0200

    Updated vim installation

--- install.sh
+++ install.sh
@@ -12,4 +12,4 @@ cargo install alacritty
 apt install neovim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-nvim -c 'PlugInstall'
+nvim +'PlugInstall --sync' +qa
